1fc8f7a70ea15479f6f1940d3319a33ae78432b8
Sam Archie <samuelarchie1@gmail.com>
2021-11-27 12:00:53 +0000

    refactored so we are mapping over the data.

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,52 @@
 import { data } from "../SpeakerData";
 
 const IndexPage = () => {
-  const { id, bio, first, last, favorite, twitterHandle, company, sessions } =
-    data[0];
-
   return (
     <div className="container speaker-list">
       <div className="row">
-        <div className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-sm-12 col-xs-12">
-          <div className="card card-height p-4 mt-4">
-            <div className="speaker-img d-flex flex-row justify-content-center align-items-center h-300">
-              <img
-                className="contain-fit"
-                src={`/images/speaker-${id}.jpg`}
-                width="300"
-              />
-            </div>
-            <div className="speaker-info">
-              <div className="d-flex justify-content-between mb-3">
-                <h3>
-                  {first} {last}
-                </h3>
+        {data.map((speaker) => {
+          const {
+            id,
+            bio,
+            first,
+            last,
+            favorite,
+            twitterHandle,
+            company,
+            sessions,
+          } = speaker;
+          return (
+            <div className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-sm-12 col-xs-12">
+              <div className="card card-height p-4 mt-4">
+                <div className="speaker-img d-flex flex-row justify-content-center align-items-center h-300">
+                  <img
+                    className="contain-fit"
+                    src={`/images/speaker-${id}.jpg`}
+                    width="300"
+                  />
+                </div>
+                <div className="speaker-info">
+                  <div className="d-flex justify-content-between mb-3">
+                    <h3>
+                      {first} {last}
+                    </h3>
+                  </div>
+                  <div>
+                    <p>
+                      {bio} {company} {twitterHandle} {favorite}
+                    </p>
+                  </div>
+                </div>
+                <div className="sessionBox card h-250">
+                  <span className="session w-100">
+                    {sessions[0].title}{" "}
+                    <storng>Room: {sessions[0].room.name}</storng>
+                  </span>
+                </div>
               </div>
-              <div>
-                <p>
-                  {bio} {company} {twitterHandle} {favorite}
-                </p>
-              </div>
             </div>
-            <div className="sessionBox card h-250">
-              <span className="session w-100">
-                {sessions[0].title}{" "}
-                <storng>Room: {sessions[0].room.name}</storng>
-              </span>
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
